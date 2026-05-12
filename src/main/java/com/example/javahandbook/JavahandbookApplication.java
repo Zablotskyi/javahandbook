dca169e676efca036b7c21b8ad268fffdef5e115
Vasyl Zablotskyi <zablotskyivv@gmail.com>
2026-05-12 22:35:04 +0300

    upload the project

---
+++ src/main/java/com/example/javahandbook/JavahandbookApplication.java
@@ -0,0 +1,13 @@
+package com.example.javahandbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavahandbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavahandbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javahandbook/JavahandbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javahandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavahandbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
